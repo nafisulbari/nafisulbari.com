c64696a73fa4c401a5297754e1b7aeec5247aadc
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2019-06-21 21:33:06 +0600

    /blog shows cats and featured blogs /blog/post/1 shows post

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,18 +8,23 @@ use App\Entity\Blogpost;
 use App\Repository\BlogpostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\Query\ResultSetMapping;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManager;
 
 class BlogController extends AbstractController {
 
 
+    public function blogPage() {
 
-
-    public function featuredBlogs() {
 
         $repository = $this->getDoctrine()->getRepository(Blogpost::class);
-        $blogs = $repository->findAll();
+        $featuredBlogs = $repository->findAll();
+        $cats = $repository->createQueryBuilder('p');
+        $c = $cats->select('p.category')->distinct()->getQuery()->getResult();
 
-        return $this->render('blog.html.twig', ['blogs' => $blogs]);
+
+        return $this->render('blog.html.twig', ['blogs' => $featuredBlogs, 'cats' => $c]);
     }
 
 
--- src/Controller/BlogPostController.php
+++ src/Controller/BlogPostController.php
@@ -7,19 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BlogPostController extends AbstractController
-{
+
+class BlogPostController extends AbstractController {
     /**
      * @Route("/blog/post", name="blog_post")
      */
-    public function createPost():Response
-    {
+    public function createPost(): Response {
         // you can fetch the EntityManager via $this->getDoctrine()
         // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
         $entityManager = $this->getDoctrine()->getManager();
 
         $blogpost = new Blogpost();
-        $blogpost->setTitle('hdd');
+        $blogpost->setTitle('ssd brah');
+        $blogpost->setCategory('science');
         $blogpost->setDateCreated(6969);
         $blogpost->setArticle('79 Nigga byte stuff');
 
@@ -29,27 +29,24 @@ class BlogPostController extends AbstractController
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
 
-        return new Response('Saved new product with id '.$blogpost->getId());
+        return new Response('Saved new product with id ' . $blogpost->getId());
     }
 
 
-    /**
-     * @Route("/blog/post/{id}", name="blog_show")
-     */
-    public function show($id)
-    {
+
+    public function show($id) {
         $blogpost = $this->getDoctrine()
             ->getRepository(Blogpost::class)
             ->find($id);
 
         if (!$blogpost) {
             throw $this->createNotFoundException(
-                'No blogs found for id '.$id
+                'No blogs found for id ' . $id
             );
         }
 
-        return new Response('Check out this great blog: '.$blogpost->getArticle());
 
+        return $this->render('blog_post/index.html.twig', ['blogpost' => $blogpost]);
         // or render a template
         // in the template, print things with {{ product.name }}
         // return $this->render('product/show.html.twig', ['product' => $product]);
