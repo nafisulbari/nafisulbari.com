3cdf2c90ed69d36c6ece64b1e5125809fda91613
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2019-07-07 04:22:57 +0600

    login implimented but after login error 500 throws

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $utils)
+    {
+        $error= $utils->getLastAuthenticationError();
+
+        $lastusername=$utils->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastusername
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){
+
+    }
+}
